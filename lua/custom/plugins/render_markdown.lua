657d1ae94a45153b9980d66c8e006b4b903227f5
rahmanhasri <rahman.hasri@gmail.com>
2025-12-09 00:48:20 +0700

    fix obsidian with markdown

--- lua/custom/plugins/render_markdown.lua
+++ lua/custom/plugins/render_markdown.lua
@@ -23,7 +23,7 @@ return {
       -- Determines how icons fill the available space:
       --  inline:  underlying text is concealed resulting in a left aligned icon
       --  overlay: result is left padded with spaces to hide any additional text
-      position = 'inline',
+      -- position = 'inline',
       unchecked = {
         -- Replaces '[ ]' of 'task_list_marker_unchecked'
         icon = '   󰄱 ',
